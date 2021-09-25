efaa4f4282f2d78eaaf57a09901982254ffa8cb9
shelleybehen <shelleybehen@gmail.com>
2021-09-25 10:46:47 +1000

    Add the ability to delete pledges

--- parentals/projects/views.py
+++ parentals/projects/views.py
@@ -88,3 +88,25 @@ class PledgeList(APIView):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
         )
+
+    def put(self, request, pk):
+        pledge = self.get_object(pk)
+        data = request.data
+        serializer = PledgeSerializer(
+            instance=pledge,data=data,partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        pledge = self.get_object(pk)
+        pledge.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
